45861b258477851a019852f398bba30559a5d6e4
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-04-15 02:58:47 +0200

    ignore last commit

--- controllers/users.js
+++ controllers/users.js
@@ -32,11 +32,10 @@ const createUser = asyncFunction(async (req, res) => {
     photo: photo,
   });
   addUserToUserBooks(user._id);
-  user.save().then(() => { res.status(200).send(user); })
+  user.save().then(() => { res.status(200).send(user); }).catch((err)=>{
+    res.status(400).send(err);
   });
-//   .catch((err)=>{
-//     res.status(400).send(err.message);
-// });
+});
 
 /// /////////////////////////////// login user ///////////////////////////////////////////
 
