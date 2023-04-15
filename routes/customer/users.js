f2c66e1493937796814e91d0a94e9f000b1718c9
hagar <hagarr.abdelzaher@gmail.com>
2023-04-16 00:19:59 +0200

    fixed update book issue

--- routes/customer/users.js
+++ routes/customer/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { usersController } = require('../../controllers/index');
+const { validation, usersValidator } = require('../../validation/validation')
 
 const router = express.Router();
 
-router.patch('/:id', usersController.updateUserById);
+router.patch('/:id',  validation(usersValidator.updateUser),usersController.updateUserById);
 router.get('/profile/', usersController.getUserById);
 module.exports = router;
